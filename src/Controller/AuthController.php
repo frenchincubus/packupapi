8e189399e402b2eb896ae0f8418cacdeef787946
orestes <orestes64@gmail.com>
2020-03-10 10:04:54 +0100

    changing to getUser

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends AbstractController
             $data = [
                 'status' => 201,
                 'message' => 'L\'utilisateur a été créé',
-                'user' => $user
+                'user' => $this->getUser();
             ];
 
             return new JsonResponse($data, 201);
