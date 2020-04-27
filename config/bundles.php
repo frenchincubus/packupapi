4245842f34222a54fc8b7856cd305a5216502063
Lionel Rénier <lionelrenier@mini-de-lionel.home>
2020-04-27 18:51:24 +0200

    create Image entity + controller + listener

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
